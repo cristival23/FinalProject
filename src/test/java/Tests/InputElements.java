8e2bb599105b816b9e83c30259805d8746640334
cristival23 <voicucristianvalentin1993@gmail.com>
2024-07-01 20:23:52 +0300

    Etapa 3

--- src/test/java/Tests/InputElements.java
+++ src/test/java/Tests/InputElements.java
@@ -2,9 +2,12 @@ package Tests;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputElements {
     public WebDriver webDriver;
@@ -35,16 +38,61 @@ public class InputElements {
         WebElement fileField = webDriver.findElement((By.name("filename")));
         fileField.sendKeys(new File("src/test/resources/3d render.jpg").getAbsolutePath());
 
+        //checkbox
+        List<String> Checkbox = Arrays.asList("cb1", "cb2");
+        List<WebElement> CheckboxList = webDriver.findElements(By.name("checkboxes[]"));
+        for (Integer index = 0; index < CheckboxList.size(); index++) {
+            String checkboxElementText = CheckboxList.get(index).getText();
+            if (Checkbox.contains(checkboxElementText)) {
+                CheckboxList.get(index).click();
+            }
+        }
+
+        //radio
+        radioField("rd2");
+
+        //multiple select
+
+        WebElement multipleSelectField = webDriver.findElement(By.name("multipleselect[]"));
+        Select select = new Select(multipleSelectField);
+        if (select.isMultiple()) {
+            select.selectByValue("ms1");
+            select.selectByValue("ms2");
+            select.selectByValue("ms3");
+        }
+        List<WebElement> selectedOptions = select.getAllSelectedOptions();
+        for (WebElement option : selectedOptions) {
+            System.out.println("Selected option: " + option.getText());
+        }
+        select.deselectByValue("ms4");
+        selectedOptions = select.getAllSelectedOptions();
+        for (WebElement option : selectedOptions) {
+            System.out.println("Selected option after deselection: " + option.getText());
+        }
+
         WebElement dropDownField = webDriver.findElement(By.name("dropdown"));
         dropDownField.sendKeys("dd5");
         dropDownField.sendKeys(Keys.ENTER);
 
-
-//
 //        WebElement submitButton = webDriver.findElement(By.xpath("//input[@type='submit']"));
 //        submitButton.click();
 
+    }
 
+    public void radioField(String radio) {
+        if (radio == "rd1") {
+            WebElement genderField = webDriver.findElement(By.cssSelector("input[name='radioval'][value='rd1']"));
+            genderField.click();
+
+        } else if (radio == "rd2") {
+            WebElement radioField = webDriver.findElement(By.cssSelector("input[name='radioval'][value='rd2']"));
+            radioField.click();
+
+        } else {
+            WebElement radioField = webDriver.findElement(By.cssSelector("input[name='radioval'][value='rd3']"));
+            radioField.click();
+
+        }
     }
 
 }
